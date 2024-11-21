a080c0be6e915e79bfdcaaaa03bb0bbb67a9ea09
lethanhtung22032005 <lethanhtungdeptrai@gmail.com>
2024-11-21 23:54:39 +0700

    Update Session2 ex5.cpp

--- Session2 ex5.cpp
+++ Session2 ex5.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main() {
-    // Khai b·o c·c bi?n chi?u d‡i v‡ chi?u r?ng
-    int length = 10; // Chi?u d‡i c?a hÏnh ch? nh?t
-    int width = 3;   // Chi?u r?ng c?a hÏnh ch? nh?t
+    // Khai b√°o c√°c bi?n chi?u d√†i v√† chi?u r?ng
+    int length = 10; // Chi?u d√†i c?a h√¨nh ch? nh?t
+    int width = 3;   // Chi?u r?ng c?a h√¨nh ch? nh?t
 
-    // TÌnh chu vi v‡ di?n tÌch c?a hÏnh ch? nh?t
+    // T√≠nh chu vi v√† di?n t√≠ch c?a h√¨nh ch? nh?t
     int perimeter = 2 * (length + width);
     int area = length * width;
 
